4262ca1b1c73151adcb8410e61054604d12e6d51
Lingxin Gu <lingxingu0112@gmail.com>
2016-10-23 15:57:10 +0800

    add user model and build reference with post

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :information
   has_many :comments
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161023023835) do
+ActiveRecord::Schema.define(version: 20161023072603) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "info"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 20161023023835) do
     t.string   "information"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
